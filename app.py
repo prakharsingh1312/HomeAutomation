f9477515be8181daa57a7e307bcb11c6fbd8223d
Prakhar Singh <prakharsingh13@gmail.com>
2020-06-27 16:10:12 +0530

    fix

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class Appliances(db.Model):
 	id=db.Column('id', db.Integer , primary_key=True)
 	name=db.Column('name' , db.String , unique=True)
 	state=db.Column('state' , db.Integer)
-	user_id=db.Column('user_id',db.Integer,db.ForeignKey(''))
+	user_id=db.Column('user_id',db.Integer,db.ForeignKey('user.id'))
 
 class Budget(db.Model):
 	__tablename__='budget'
@@ -19,19 +19,19 @@ class Budget(db.Model):
 	#payment_method=db.Column('payment_method',db.Integer)
 	amount=db.Column('amount',db.Float)
 	timestamp=db.Column('timestamp',db.timestamp);
-	#user=db.Column('user_id',db.Integer)
+	user_id=db.Column('user_id',db.Integer,db.ForeignKey('user.id'))
 
 class BudgetTypes(db.Model):
 	__tablename__='budget_types'
 	id=db.Column('id', db.Integer , primary_key=True)
 	name=db.Column('name',db.String,unique=True)
-	#user=db.Column('user_id',db.Integer)
+	user_id=db.Column('user_id',db.Integer,db.ForeignKey('user.id'))
 
 class BudgetPaymentMethod(db.Model):
 	__tablename__='budget_payment_method'
 	id=db.Column('id', db.Integer , primary_key=True)
 	name=db.Column('name',db.String,unique=True)
-	#user=db.Column('user_id',db.Integer)
+	user_id=db.Column('user_id',db.Integer,db.ForeignKey('user.id'))
 	
 
 class UserTable(db.model):
@@ -40,6 +40,12 @@ class UserTable(db.model):
     name=db.Column('name' , db.String)
     password=db.Column('password' , db.String)
     email=db.Column('email' , db.String)
+	appliances=db.relationship('Appliances',backref='owner')
+	budgets=db.relationship('Budget',backref='owner')
+	budget_types=db.relationship('BudgetTypes',backref='owner')
+	budget_payment_methods=db.relationship('BudgetPaymentMethod',backref='owner')
+	
+	
 
 class ReminderAlarm(db.model):
     __tablename__='ReminderAlarm'
@@ -49,6 +55,7 @@ class ReminderAlarm(db.model):
     frequency=db.Column('frequency' , db.Integer)
     time=db.Column('time' , db.Time)
     day=db.Column('day' , db.Date)
+	
 
 class Automation(db.model):
     __tablename__='Automation'
@@ -64,7 +71,7 @@ class AutomationParameter(db.model):
    	name=db.Column('name' , db.String , unique=True)
     min=db.Column('min value' , db.Integer)
     max=db.Column('max value' , db.Integer)
-#db.create_all()
+db.create_all()
 #db.drop_all()
 
 @app.route("/")
