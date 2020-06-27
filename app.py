ead1798d809591964d81aa73f4a81895252b2644
Prakhar Singh <prakharsingh13@gmail.com>
2020-06-27 15:29:12 +0530

    added more tables

--- app.py
+++ app.py
@@ -9,8 +9,29 @@ class Appliances(db.Model):
 	appliance_id=db.Column('id', db.Integer , primary_key=True)
 	appliance_name=db.Column('name',db.String)
 	appliance_state=db.Column('state',db.Integer)
+	#appliance_user_id=db.Column('user_id',db.Integer)
 
+class Budget(db.Model):
+	__tablename__='budget'
+	budget_id=db.Column('id', db.Integer , primary_key=True)
+	#budget_type=db.Column('type',db.Integer)
+	budget_desc=db.Column('desc',db.String)
+	#budget_payment_method=db.Column('payment_method',db.Integer)
+	budget_amount=db.Column('amount',db.Float)
+	budget_timestamp=db.Column('timestamp',db.timestamp);
+	#budget_user=db.Column('user_id',db.Integer)
 
+class BudgetTypes(db.Model):
+	__tablename__='budget_types'
+	budget_type_id=db.Column('id', db.Integer , primary_key=True)
+	budget_type_name=db.Column('name',db.String,unique=True)
+	#budget_type_user=db.Column('user_id',db.Integer)
+
+class BudgetPaymentMethod(db.Model):
+	__tablename__='budget_payment_method'
+	budget_payment_method_id=db.Column('id', db.Integer , primary_key=True)
+	budget_payment_method_name=db.Column('name',db.String,unique=True)
+	#budget_payment_method_user=db.Column('user_id',db.Integer)
 
 @app.route("/")
 def hello():
