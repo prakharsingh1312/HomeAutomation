1624704d051901ea2d7abaeca06999f6d8791613
Prakhar Singh <prakharsingh13@gmail.com>
2020-06-27 16:37:02 +0530

    fix

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class Budget(db.Model):
 	desc=db.Column('desc',db.String)
 	#payment_method=db.Column('payment_method',db.Integer)
 	amount=db.Column('amount',db.Float)
-	timestamp=db.Column('timestamp',db.timestamp);
+	timestamp=db.Column('timestamp',db.DateTime);
 	user_id=db.Column('user_id',db.Integer,db.ForeignKey('user.id'))
 
 class BudgetTypes(db.Model):
@@ -34,20 +34,20 @@ class BudgetPaymentMethod(db.Model):
 	user_id=db.Column('user_id',db.Integer,db.ForeignKey('user.id'))
 	
 
-class UserTable(db.model):
-    __tablename__='user'
-    id=db.Column('id' , db.Integer , primary_key=True)
-    name=db.Column('name' , db.String)
-    password=db.Column('password' , db.String)
-    email=db.Column('email' , db.String)
-	appliances=db.relationship('Appliances',backref='owner')
-	budgets=db.relationship('Budget',backref='owner')
-	budget_types=db.relationship('BudgetTypes',backref='owner')
-	budget_payment_methods=db.relationship('BudgetPaymentMethod',backref='owner')
+class UserTable(db.Model):
+	__tablename__='user'
+	id=db.Column('id' , db.Integer , primary_key=True)
+	name=db.Column('name' , db.String)
+	password=db.Column('password' , db.String)
+	email=db.Column('email' , db.String)
+	appliances=db.relationship('Appliances', backref='owner')
+	budgets=db.relationship('Budget', backref='owner')
+	budget_types=db.relationship('BudgetTypes', backref='owner')
+	budget_payment_methods=db.relationship('BudgetPaymentMethod', backref='owner')
 	
 	
 
-class ReminderAlarm(db.model):
+class ReminderAlarm(db.Model):
     __tablename__='ReminderAlarm'
     id=db.Column('id' , db.Integer, primary_key=True)
     type=db.Column('alert type' , db.Integer)
@@ -57,7 +57,7 @@ class ReminderAlarm(db.model):
     day=db.Column('day' , db.Date)
 	
 
-class Automation(db.model):
+class Automation(db.Model):
     __tablename__='Automation'
     id=db.Column("id" , db.Integer , primary_key=True)
     # parameter_id=db.Column('parameter_id')
@@ -65,12 +65,12 @@ class Automation(db.model):
     # appliance_state_value=db.Column('state value' , db.Integer)
     # appliance_state_value - Tells us about when the appliance will ON/OFF
 
-class AutomationParameter(db.model):
-    __tablename='Parameter'
-    id=db.Column('id' , db.Integer , primary_key=True)
-   	name=db.Column('name' , db.String , unique=True)
-    min=db.Column('min value' , db.Integer)
-    max=db.Column('max value' , db.Integer)
+class AutomationParameter(db.Model):
+	__tablename='Parameter'
+	id=db.Column('id' , db.Integer , primary_key=True)
+	name=db.Column('name' , db.String , unique=True)
+	min=db.Column('min value' , db.Integer)
+	max=db.Column('max value' , db.Integer)
 db.create_all()
 #db.drop_all()
 
