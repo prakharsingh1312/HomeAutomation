c80cbf9f856c481d74bdef88eab6b706a2b2f4ea
Ritik Rawat <ritikneverback@gmail.com>
2020-06-29 20:09:41 +0530

    fix

--- functions.py
+++ functions.py
@@ -81,8 +81,8 @@ def verify(token, hash):
 	user=UserTable.query.filter_by(hash=hash).first()
 	user_email=crypt_password(user.email)
 	if token == user_email:
-		user.activated=1
-		user.hash=''
+		user.user_activated=1
+		user.user_hash=''
 		db.session.commit()
 		return 4
 	return 0
